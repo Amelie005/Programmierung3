12206fc12b110cb9ee46ff9a5c0be1261b19d1c9
Amelie <dzierzawaamelie@gmail.com>
2026-03-29 14:07:39 +0200

    Zensurenspiegel

--- Algorithmik/src/main/java/Zensurenspiegel/Main.java
+++ Algorithmik/src/main/java/Zensurenspiegel/Main.java
@@ -1,4 +1,52 @@
 package Zensurenspiegel;
 
+import java.util.Scanner;
+
+/**
+ * Einstiegspunkt des Zensurenspiegelprogramms
+ */
 public class Main {
+    /**
+     * Startet die Eingabe und gibt den Zensurenspiegel aus
+     * @param args Kommandozeilenargumente (werden nicht verwendet)
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(".---------------.\n" +
+                "|Zensurenspiegel|\n" +
+                "'---------------'");
+
+        System.out.println("Was ist die höchste Note?: ");
+        int maxNote = scanner.nextInt();
+
+        Zensurenspiegel z = new Zensurenspiegel(maxNote);
+
+        System.out.println("Geben sie die Noten ein (" + (maxNote +1) + " zum beenden): " );
+
+        int note;
+
+        do {
+            System.out.print("Note: ");
+            note = scanner.nextInt();
+
+            if (note != maxNote + 1) {
+                try {
+                    z.noteEintragen(note);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Ungültige Note! Bitte 1-5 oder 6 zum Beenden.");
+                }
+            }
+
+        } while (note != maxNote + 1);
+
+        System.out.println("\nZensurenspiegel:");
+        for (int i = 1; i <= maxNote; i++) {
+            System.out.println("Note " + i + " kam " + z.getAnzahlNote(i) + " mal vor.");
+        }
+
+        System.out.println();
+        System.out.println("Der Notendurchschnitt beträgt: Ø" + String.format("%.2f", z.getDurchschnitt()));
+
+    }
 }
--- Algorithmik/src/main/java/Zensurenspiegel/Zensurenspiegel.java
+++ Algorithmik/src/main/java/Zensurenspiegel/Zensurenspiegel.java
@@ -1,4 +1,65 @@
 package Zensurenspiegel;
 
+/**
+ * Verwaltet den Notenspiegel einer Klausur
+ * @author Amelie Sophie Dzierzawa, 599428
+ */
 public class Zensurenspiegel {
+
+    /**
+     * Konstante, die die höchste Note repräsentiert
+     */
+    private final int maxNote;
+    private final int[] anzahlNoten;
+
+    /**
+     * Konstruktor, der die höchste mögliche Note als Parameter nimmt und
+     * ein neues Array erstellt, mit denen die Anzahl der Notenvorkommen gespeichert werden kann
+     * @param maxNote höchstmögliche Note
+     */
+    public Zensurenspiegel(int maxNote) {
+        this.maxNote = maxNote;
+        this.anzahlNoten = new int[maxNote];
+    }
+
+    /**
+     * Trägt eine Note in den Zensurenspiegel ein
+     * @param note Note die eingetragen werden soll (1 - höchste Note)
+     * @throws IllegalArgumentException wenn die Note höchste Note + 1 ist
+     */
+    public void noteEintragen(int note) {
+        if (note < 1 || note > maxNote)
+            throw new IllegalArgumentException("Note muss zwischen 1 und 5 liegen!");
+        anzahlNoten[note - 1]++;  // Note 1 -> Index 0, Note 2 -> Index 1, usw.
+    }
+
+    /**
+     * Gibt zurück wie oft eine bestimmte Note vorkommt
+     * @param note die gesuchte Note (1 - höchste Note)
+     * @return Anzahl des Vorkommens der Note
+     */
+    public int getAnzahlNote(int note) {
+        return anzahlNoten[note - 1];
+    }
+
+    /**
+     * Berechnet den Notendurchschnitt aller eingetragenen Noten
+     * @return Durchschnitt aller Noten
+     * @throws ArithmeticException wenn noch keine Noten eingetragen wurden
+     */
+    public double getDurchschnitt() {
+        int summe = 0;
+        int anzahl = 0;
+
+        for (int i = 0; i < anzahlNoten.length; i++) {
+            summe += (i + 1) * anzahlNoten[i];  // Note * Anzahl ihres Vorkommens
+            anzahl += anzahlNoten[i];            // Gesamtanzahl aller Studenten
+        }
+
+        if (anzahl == 0)
+            throw new ArithmeticException("Es wurden noch keine Noten eingetragen!");
+
+        return (double) summe / anzahl;
+    }
+
 }
